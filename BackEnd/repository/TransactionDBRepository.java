9210973010e79736c1d95fe60113d3e3da04da5e
Yamini Dongaonkar <76465645+Yamini2391@users.noreply.github.com>
2022-09-25 22:30:13 +0530

    Delete BackEnd/repository directory

--- BackEnd/repository/TransactionDBRepository.java
+++
@@ -1,9 +0,0 @@
-package com.citi.sanction_transaction.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.citi.sanction_transaction.model.TransactionDB;
-
-public interface TransactionDBRepository extends JpaRepository<TransactionDB, Integer> {
-	
-}
